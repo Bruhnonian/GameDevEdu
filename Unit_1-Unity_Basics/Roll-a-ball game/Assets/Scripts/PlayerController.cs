0c267df18c8b430937bb5ce24e0a105224980540
Bruhnonian <aminoo15@hotmail.com>
2023-10-19 05:20:28 +0400

    roll a ball game tutorial part 2

--- Unit_1-Unity_Basics/Roll-a-ball game/Assets/Scripts/PlayerController.cs
+++ Unit_1-Unity_Basics/Roll-a-ball game/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
      private Rigidbody rb; 
+     private int count;
+     public GameObject winTextObject;
+
+
      private float movementX;
      private float movementY;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -24,13 +34,54 @@ public class PlayerController : MonoBehaviour
        movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+      countText.text = "Count: " + count.ToString();
+      if(count >= 12)
+      {
+        winTextObject.SetActive(true);
+        
+        //Destroy Enemy
+        Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+      }
+
+    }
+
     void FixedUpdate()
     {
       Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
       rb.AddForce(movement * speed);
+      
 
       
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+      if(other.gameObject.CompareTag("PickUp"))
+      {
+        
+        other.gameObject.SetActive(false);
+        count = count +1;
+
+        SetCountText(); 
+
+      }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+      if (collision.gameObject.CompareTag("Enemy"))
+      {
+        // destroy the current Object
+        Destroy(gameObject);
+        // Set the text to "You Lose"
+        winTextObject.gameObject.SetActive(true);
+        winTextObject.GetComponent<TextMeshProUGUI>().text = "You lose!";
+      }
+
+    }
 }
+
 
